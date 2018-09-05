63bf6c4cfc76af5c3a2dff07b2ecfead8e5e2890
Kendra Kuhl <kendra@kuhl.ws>
2018-09-05 18:30:58 -0500

    Initial Commit

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,7 +6,55 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* Create and initialize in a single line an array with the following values: 1, 1, 2, 3, 5, 8. 
+             * Then loop through the array and print out each value. */
+            var numbers = new int[] { 1, 1, 2, 3, 5, 8 };
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+            Console.Clear();
+
+            /* Count how many odd numbers are in a list. */
+            var oddNumberArray = new int[] { 1, 1, 2, 3, 5, 8 };
+            var numberOfOddNumbers = 0; // This is my counter
+
+            foreach (var number in oddNumberArray)
+            {
+                // If the modulus (aka remainder) is more than zero, add one to our counter
+                if ((number % 2) > 0)
+                {
+                    Console.WriteLine(number);
+                    numberOfOddNumbers++;
+                }
+            }
+
+            Console.WriteLine($"Count of odd numbers: {numberOfOddNumbers}");
+
+            Console.ReadLine();
+            Console.Clear();
+
+            /*Sum up all the negative numbers in a list. */
+            var negativeNumberArray = new int[] { 1, -1, 2, 3, -5, 8 };
+            var sumOfNegativeNumbers = 0; // This is my sum variable
+
+            foreach (var number in negativeNumberArray)
+            {
+                // If the number is negative, then add to the sum variable
+                if (number < 0)
+                {
+                    Console.WriteLine(number);
+                    sumOfNegativeNumbers += number; 
+                }
+            }
+
+            Console.WriteLine($"Sum of negative numbers: {sumOfNegativeNumbers}");
+
+            Console.ReadLine();
+            Console.Clear();
+
         }
     }
 }
